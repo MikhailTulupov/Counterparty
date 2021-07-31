4073a00c0963cfa7390da1311ae5a103a08dc411
Mikhail <kd.maikl-201@yandex.ru>
2021-07-31 19:48:18 +0700

    Create counterparty model with a link to the room

---
+++ app/src/main/java/mikhail/tulupov/application/counterparty/data/models/Counterparty.kt
@@ -0,0 +1,23 @@
+package mikhail.tulupov.application.counterparty.data.models
+
+import android.graphics.Bitmap
+import androidx.room.*
+import mikhail.tulupov.application.counterparty.utilities.*
+import java.util.*
+
+@Entity(tableName = DATABASE_TABLE_NAME)
+data class Counterparty(
+    @PrimaryKey
+    @ColumnInfo(name = COLUMN_ID, typeAffinity = ColumnInfo.TEXT)
+    var id: UUID = setId(),
+    @ColumnInfo(name = COLUMN_NAME, typeAffinity = ColumnInfo.TEXT)
+    var name: String = "",
+    @ColumnInfo(name = COLUMN_PHONE_NUMBER, typeAffinity = ColumnInfo.TEXT)
+    var phoneNumber: String = "",
+    @ColumnInfo(name = COLUMN_EMAIL, typeAffinity = ColumnInfo.TEXT)
+    var email: String = "",
+    @ColumnInfo(name = COLUMN_AVATAR,typeAffinity = ColumnInfo.BLOB)
+    var avatar: Bitmap? = null
+)
+
+private fun setId() = UUID.randomUUID()
