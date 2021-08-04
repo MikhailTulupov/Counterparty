fd14c1d90aaf6f2e6954f84879b3e3713be4404f
Mikhail <kd.maikl-201@yandex.ru>
2021-08-04 10:10:34 +0700

    Add return types and add suspend key world

--- app/src/main/java/mikhail/tulupov/application/counterparty/data/repositories/CounterpartyRepository.kt
+++ app/src/main/java/mikhail/tulupov/application/counterparty/data/repositories/CounterpartyRepository.kt
@@ -1,6 +1,5 @@
 package mikhail.tulupov.application.counterparty.data.repositories
 
-import android.app.Application
 import android.content.Context
 import mikhail.tulupov.application.counterparty.data.database.CounterpartyDatabase
 import mikhail.tulupov.application.counterparty.data.models.Counterparty
@@ -9,19 +8,19 @@ import java.util.*
 class CounterpartyRepository private constructor(context: Context) {
 
     private val counterpartyDao =
-        CounterpartyDatabase.getInstance(context).counterpartyDao()
+        CounterpartyDatabase.getDatabase(context).counterpartyDao()
 
     fun getAllCounterparty() = counterpartyDao.getAllCounterparty()
 
     fun getCounterpartyById(id: UUID) = counterpartyDao.getCounterpartyById(id)
 
-    fun addCounterparty(counterparty: Counterparty) =
+    suspend fun addCounterparty(counterparty: Counterparty) =
         counterpartyDao.insertCounterparty(counterparty)
 
-    fun updateCounterparty(counterparty: Counterparty) =
+    suspend fun updateCounterparty(counterparty: Counterparty) =
         counterpartyDao.updateCounterparty(counterparty)
 
-    fun deleteCounterparty(counterparty: Counterparty) =
+    suspend fun deleteCounterparty(counterparty: Counterparty) =
         counterpartyDao.deleteCounterparty(counterparty)
 
     companion object {
