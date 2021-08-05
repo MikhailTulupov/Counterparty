770904b9a40afde902a798843a3992514535f364
Mikhail <kd.maikl-201@yandex.ru>
2021-08-05 19:09:51 +0700

    Setup action bar

--- app/src/main/java/mikhail/tulupov/application/counterparty/ui/activites/CounterpartyActivity.kt
+++ app/src/main/java/mikhail/tulupov/application/counterparty/ui/activites/CounterpartyActivity.kt
@@ -3,6 +3,9 @@ package mikhail.tulupov.application.counterparty.ui.activites
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import mikhail.tulupov.application.counterparty.R
 import mikhail.tulupov.application.counterparty.databinding.ActivityCounterpartyBinding
 
@@ -16,5 +19,18 @@ class CounterpartyActivity : AppCompatActivity() {
         binding =
             DataBindingUtil.setContentView(this, R.layout.activity_counterparty)
 
+        // find navHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+        val navController = navHostFragment?.findNavController()
+        // setup action bar
+        NavigationUI.setupActionBarWithNavController(this, navController!!)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        super.onSupportNavigateUp()
+        val navController = this.findNavController(R.id.nav_host_fragment_container)
+        return navController.navigateUp()
     }
 }
