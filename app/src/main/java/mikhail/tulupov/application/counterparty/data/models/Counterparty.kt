c7692e78c6d48fba5b8065e3d38736c1e1dd3f21
Mikhail <kd.maikl-201@yandex.ru>
2021-08-05 17:17:20 +0700

    Add factory and change default variables

--- app/src/main/java/mikhail/tulupov/application/counterparty/data/models/Counterparty.kt
+++ app/src/main/java/mikhail/tulupov/application/counterparty/data/models/Counterparty.kt
@@ -3,6 +3,7 @@ package mikhail.tulupov.application.counterparty.data.models
 import android.graphics.Bitmap
 import androidx.room.*
 import mikhail.tulupov.application.counterparty.utilities.*
+import mikhail.tulupov.application.counterparty.utilities.Utils.setId
 import java.util.*
 
 @Entity(tableName = DATABASE_TABLE_NAME)
@@ -11,29 +12,53 @@ data class Counterparty(
     @ColumnInfo(name = COLUMN_ID, typeAffinity = ColumnInfo.TEXT)
     var id: UUID = setId(),
     @ColumnInfo(name = COLUMN_NAME, typeAffinity = ColumnInfo.TEXT)
-    var name: String = "",
+    var name: String,
     @ColumnInfo(name = COLUMN_PHONE_NUMBER, typeAffinity = ColumnInfo.TEXT)
-    var phoneNumber: String = "",
+    var phoneNumber: String,
     @ColumnInfo(name = COLUMN_EMAIL, typeAffinity = ColumnInfo.TEXT)
-    var email: String = "",
+    var email: String? = null,
     @ColumnInfo(name = COLUMN_AVATAR, typeAffinity = ColumnInfo.BLOB)
     var avatar: Bitmap? = null
 ) {
-    constructor(name: String, phoneNumber: String, email: String) : this(
-        id = setId(),
-        name,
-        phoneNumber,
-        email,
-        avatar = null
-    )
 
-    constructor(name: String, phoneNumber: String, email: String, avatar: Bitmap?) : this(
-        id = setId(),
-        name,
-        phoneNumber,
-        email,
-        avatar
-    )
+
+    companion object {
+
+    }
+
+    object CounterpartyFactory {
+
+        fun makeCounterparty(name: String, phoneNumber: String) = Counterparty(
+            id = setId(),
+            name,
+            phoneNumber,
+            email = "",
+            avatar = null
+        )
+
+        fun makeCounterparty(name: String, phoneNumber: String, email: String?) = Counterparty(
+            id = setId(),
+            name,
+            phoneNumber,
+            email,
+            avatar = null
+        )
+
+        fun makeCounterparty(name: String, phoneNumber: String, email: String?, avatar: Bitmap?) =
+            Counterparty(
+                id = setId(),
+                name,
+                phoneNumber,
+                email,
+                avatar
+            )
+
+        fun makeCounterparty(avatar: Bitmap?) = Counterparty(
+            id = setId(),
+            name = "",
+            phoneNumber = "",
+            email = "",
+            avatar
+        )
+    }
 }
-
-private fun setId() = UUID.randomUUID()
