c81b7bd5ccbc3d0382474bb1b359f04044477e33
Mikhail <kd.maikl-201@yandex.ru>
2021-08-05 18:51:08 +0700

    Change code More pragmatic code. Add comments. Refactoring code.

--- app/src/main/java/mikhail/tulupov/application/counterparty/ui/adapters/ListCounterpartyAdapter.kt
+++ app/src/main/java/mikhail/tulupov/application/counterparty/ui/adapters/ListCounterpartyAdapter.kt
@@ -11,39 +11,61 @@ import androidx.recyclerview.widget.RecyclerView
 import mikhail.tulupov.application.counterparty.R
 import mikhail.tulupov.application.counterparty.data.models.Counterparty
 import mikhail.tulupov.application.counterparty.databinding.ListItemCounterpartyBinding
+import mikhail.tulupov.application.counterparty.ui.adapters.ListCounterpartyAdapter.ListCounterpartyViewHolder
 
-class ListCounterpartyAdapter(onclick: () -> Unit) :
-    ListAdapter<Counterparty, ListCounterpartyViewHolder>(CounterpartyDiffCallback()) {
+class ListCounterpartyAdapter(private val onClickListener: (Counterparty) -> Unit) :
+    ListAdapter<Counterparty, ListCounterpartyViewHolder>(
+        COUNTERPARTY_COMPARATOR) {
 
+    // override this fun for sort list
+    override fun submitList(list: MutableList<Counterparty>?) {
+        super.submitList(list)
+        list?.sortBy { it.name.lowercase() }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListCounterpartyViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = DataBindingUtil.inflate<ListItemCounterpartyBinding>(
-            inflater,
-            R.layout.list_item_counterparty, parent, false
-        )
-
-        return ListCounterpartyViewHolder(binding.root)
+        return ListCounterpartyViewHolder.onCreate(parent)
     }
 
     override fun onBindViewHolder(holder: ListCounterpartyViewHolder, position: Int) {
         val counterparty = getItem(position)
-        val binding = holder.binding
-
-        binding?.ivAvatar?.setImageBitmap(counterparty.avatar)
-        binding?.tvName?.text = SpannableStringBuilder(counterparty.name)
+        holder.bind(counterparty, onClickListener)
     }
 
-}
+    class ListCounterpartyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding: ListItemCounterpartyBinding? = DataBindingUtil.bind(itemView)
 
-class ListCounterpartyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val binding: ListItemCounterpartyBinding? = DataBindingUtil.bind(itemView)
-}
+        // binding data and layout
+        fun bind(counterparty: Counterparty, onClickListener: (Counterparty) -> Unit) {
+            binding?.apply {
+                ivAvatar.setImageBitmap(counterparty.avatar)
+                tvName.text = SpannableStringBuilder(counterparty.name)
+                root.setOnClickListener { onClickListener(counterparty) }
+            }
+        }
 
-class CounterpartyDiffCallback : DiffUtil.ItemCallback<Counterparty>() {
-    override fun areItemsTheSame(oldItem: Counterparty, newItem: Counterparty): Boolean =
-        oldItem.id == newItem.id
+        companion object {
+            // this fun pre-pair view holder and return view holder
+            fun onCreate(parent: ViewGroup): ListCounterpartyViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val binding = DataBindingUtil.inflate<ListItemCounterpartyBinding>(
+                    inflater,
+                    R.layout.list_item_counterparty, parent, false
+                )
+                return ListCounterpartyViewHolder(binding.root)
+            }
+        }
+    }
 
-    override fun areContentsTheSame(oldItem: Counterparty, newItem: Counterparty): Boolean =
-        oldItem == newItem
+    companion object {
+        //  calculate the difference between two lists and output a list of update operations
+        //  that converts the first list into the second one.
+        private  val COUNTERPARTY_COMPARATOR = object : DiffUtil.ItemCallback<Counterparty>() {
+            override fun areItemsTheSame(oldItem: Counterparty, newItem: Counterparty): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Counterparty, newItem: Counterparty): Boolean =
+                oldItem == newItem
+        }
+    }
 }
